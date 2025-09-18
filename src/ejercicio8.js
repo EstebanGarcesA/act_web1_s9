b31e19d84c0df0d30ba2349995ac856b4d71b772
EstebanGarcesA <esteg254@gmail.com>
2025-09-17 19:34:01 -0500

    Agrega archivos HTML y JavaScript para ejercicios de manipulación del DOM, incluyendo cambios de contenido, atributos y eventos.

---
+++ src/ejercicio8.js
@@ -0,0 +1,21 @@
+        const caja1 = document.getElementById('caja1');
+        const caja2 = document.getElementById('caja2');
+        const mensaje = document.getElementById('mensaje');
+        
+        
+        caja1.addEventListener('click', function() {
+            caja1.style.backgroundColor = 'red';
+            mensaje.textContent = '¡Hiciste click!';
+        });
+        
+      
+        caja2.addEventListener('mouseover', function() {
+            caja2.style.backgroundColor = 'green';
+            mensaje.textContent = 'Mouse encima';
+        });
+        
+      
+        caja2.addEventListener('mouseout', function() {
+            caja2.style.backgroundColor = 'lightblue';
+            mensaje.textContent = 'Mouse fuera';
+        });
